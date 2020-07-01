436e66568563c57ed828a284c718429645b225b6
gh0stwizard <vitaliy.tokarev@gmail.com>
2020-07-01 21:59:12 +0300

    t4: added ASN templates

---
+++ YaWhois/Data/as32.cs
@@ -0,0 +1,29 @@
+﻿//
+// Auto-generated code. Don't edit.
+//
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace YaWhois.Data
+{
+    public partial class Assignments
+    {
+        /// <summary>
+        /// Format: First / Last / Server.
+        /// </summary>
+        public static readonly Tuple<uint, uint, string>[] AS32 = {
+            Tuple.Create(131077U, 131086U, "whois.nic.ad.jp"),
+            Tuple.Create(131092U, 131101U, "whois.nic.or.kr"),
+            Tuple.Create(131152U, 131161U, "whois.nic.ad.jp"),
+            Tuple.Create(131791U, 131890U, "whois.nic.or.kr"),
+            Tuple.Create(131893U, 131992U, "whois.nic.ad.jp"),
+            Tuple.Create(131072U, 196607U, "whois.apnic.net"),
+            Tuple.Create(196608U, 262143U, "whois.ripe.net"),
+            Tuple.Create(262144U, 327679U, "whois.lacnic.net"),
+            Tuple.Create(327680U, 393215U, "whois.afrinic.net"),
+            Tuple.Create(393216U, 458751U, "whois.arin.net"),
+            Tuple.Create(4200000000U, 4294967294U, "whois.ripe.net"),
+        };
+    }
+}
