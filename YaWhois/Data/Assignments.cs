6cd026e7a052ef77194bb5d9350b453a44a9e0d2
gh0stwizard <vitaliy.tokarev@gmail.com>
2020-07-01 22:00:04 +0300

    t4: added tld templates

---
+++ YaWhois/Data/Assignments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaWhois.Data
+{
+    public partial class Assignments
+    {
+        public struct TldOptions
+        {
+            public Hints Hint;
+            public string Server;
+        }
+
+        public enum Hints
+        {
+            NONE,
+            EXTERNAL, //x01
+            NOSERVER, //0x03
+            CSRNIC, //x04
+            AFILIAS, //x08
+            IPv4, //x0c
+            IPv6, //x0d
+        }
+    }
+}
