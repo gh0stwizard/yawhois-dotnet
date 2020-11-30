5d46dd60e40cb1ab9811a41bdbb5c9f4dadb2c1d
gh0stwizard <vitaliy.tokarev@gmail.com>
2020-11-30 13:55:53 +0300

    updated tests of nic handles

--- YaWhois.Tests/QueryParser/GuessServer/NicHandles.cs
+++ YaWhois.Tests/QueryParser/GuessServer/NicHandles.cs
@@ -5,13 +5,32 @@ namespace YaWhois.Tests.QueryParser.GuessServer
 {
     public class NicHandles : BaseClass
     {
-        [TestCase("CREW-RIPE")]
-        [TestCase("RIPE-NCC-HM-MNT")]
         [TestCase("NET-148-59-244-0-1")]
-        public void Passed(string value)
+        public void NoDotButHyphen_ARIN(string value)
         {
             var qp = _parser.GuessServer(value);
             Assert.AreEqual(value, qp.Query);
+            Assert.AreEqual("whois.arin.net", qp.Server);
+        }
+
+
+        // Assignments.NicHandleSuffixes
+        [TestCase("CREW-RIPE")]
+        [TestCase("RIPE-NCC-HM-MNT")]
+        public void NoDotButHyphen_RIPE(string value)
+        {
+            var qp = _parser.GuessServer(value);
+            Assert.AreEqual(value, qp.Query);
+            Assert.AreEqual("whois.ripe.net", qp.Server);
+        }
+
+
+        // NSI NIC handle
+        [TestCase("!nsi")]
+        public void ExclamationMark(string value)
+        {
+            var qp = _parser.GuessServer(value);
+            Assert.AreEqual("whois.networksolutions.com", qp.Server);
         }
     }
 }
